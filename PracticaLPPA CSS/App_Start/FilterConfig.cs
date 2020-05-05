0451c717ee2ce7e29b36936d88c0a62dbf4281ca
Carla Rossi Bergara <carla.rossib@gmail.com>
2020-05-05 00:18:53 -0300

    CreacionEstructuraCss

---
+++ PracticaLPPA CSS/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace PracticaLPPA_CSS
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ PracticaLPPA CSS/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PracticaLPPA_CSS.MvcApplication" Language="C#" %>

41827396f9f85959ccce9a40f4d6a7f200157174
Carla Rossi Bergara <carla.rossib@gmail.com>
2020-05-18 16:13:52 -0300

    EjercicioListarJS

---
+++ PracticaJavaScript/JavaScriptPractica/Controllers/JavaScriptController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JavaScriptPractica.Controllers
+{
+    public class JavaScriptController : Controller
+    {
+        // GET: JavaScript
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Variables()
+        {
+            return View();
+        }
+
+        public ActionResult Condicionales()
+        {
+            return View();
+        }
+
+        public ActionResult Funciones()
+        {
+            return View();
+        }
+
+    }
+}
